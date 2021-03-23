6dc048d525ca182b56ae307da7d010885116aa97
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-03-24 00:46:35 +0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { FormsModule } from '@angular/forms';
+import { StructureComponent } from './structure/structure.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { HistoryComponent } from './history/history.component';
+import { FilesComponent } from './files/files.component';
+import { MainlyComponent } from './mainly/mainly.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StructureComponent,
+    DocumentsComponent,
+    HistoryComponent,
+    FilesComponent,
+    MainlyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
